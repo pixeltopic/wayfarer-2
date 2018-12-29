28750d4e6f303c930a912f02bda07cc87c5c2723
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2018-12-29 01:47:59 -0800

    Created frontend of signup page with media queries

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Formik, Form, ErrorMessage } from "formik";
-import  { Button, Form as SemForm } from "semantic-ui-react";
+import  { Button, Form as SemForm, Segment, Header, Icon } from "semantic-ui-react";
 import * as Yup from "yup";
+import MediaQuery from "react-responsive";
 
 import SemanticField from "../helpers/SemanticField";
 
@@ -14,10 +15,19 @@ class Signup extends Component {
 
   validateSchema = () => (
     Yup.object().shape({ 
-      email: Yup.string().email("Not a valid email.").required("You an email!"),
+      email: Yup.string().email("Not a valid email.").required("You need an email!"),
       password: Yup.string().min(5, "Too short!").required("You need a password!")
     })
   );
+
+  validateForm = (values) => {
+    const errors = {};
+    if (values.password !== values.confirmPassword) {
+      errors.confirmPassword = "Your password doesn't match!";
+    }
+
+    return errors;
+  }
 
   renderError = props => {
     console.log(props);
@@ -26,26 +36,61 @@ class Signup extends Component {
 
   renderForm = ({ isSubmitting }) => {
     return (
-      <Form>
-        <SemanticField type="text" fluid component={SemForm.Input} name="email" placeholder="email" />
-        <ErrorMessage name="email" component={this.renderError} />
-        <SemanticField type="password" fluid component={SemForm.Input} name="password" placeholder="*****" />
-        <ErrorMessage name="password" component={this.renderError} />
-        <Button type="submit" disabled={isSubmitting}>
-          Submit
-        </Button>
-
-
-      </Form>
+      <Segment stacked padded="very">
+        <div style={{ textAlign: "center" }}>
+          <Header as="h2" color="teal" icon>
+            <Icon name="user circle" />
+            Sign Up for an account
+          </Header>
+        </div>
+        <Form>
+          <SemanticField type="text" fluid icon="user" iconPosition="left" component={SemForm.Input} name="email" placeholder="email" />
+          <ErrorMessage name="email" component={this.renderError} />
+          <SemanticField type="password" fluid icon="lock" iconPosition="left" component={SemForm.Input} name="password" placeholder="password" />
+          <ErrorMessage name="password" component={this.renderError} />
+          <SemanticField type="password" fluid icon="lock" iconPosition="left" component={SemForm.Input} name="confirmPassword" placeholder="confirm password" />
+          <ErrorMessage name="confirmPassword" component={this.renderError} />
+          <Button 
+            type="submit" 
+            style={{ marginTop: "20px" }} 
+            loading={isSubmitting}
+            disabled={isSubmitting}
+            className="teal" 
+            fluid 
+            size="large"
+          >
+            Submit
+          </Button>
+        </Form>
+      </Segment>
     );
+  }
+
+  renderWithQuery = props => {
+    return (
+      <React.Fragment>
+        <MediaQuery minWidth={700}>
+          <div style={{ width: "50%", margin: "auto", padding: "70px 0" }}>
+            {this.renderForm(props)}
+          </div>
+        </MediaQuery>
+        <MediaQuery maxWidth={700}>
+          <div style={{ width: "80%", margin: "auto", padding: "70px 0" }}>
+            {this.renderForm(props)}
+          </div>
+        </MediaQuery>
+      </React.Fragment>
+    );
+
   }
 
   render() {
     return (
       <Formik 
-        render={this.renderForm}
-        initialValues={{ email: "", password: "" }}
+        render={this.renderWithQuery}
+        initialValues={{ email: "", password: "", confirmPassword: "" }}
         validationSchema={this.validateSchema}
+        validate={this.validateForm}
         onSubmit={this.onSubmit}
       />
     );

d0fdc1b1384464d6e7af66640902f2c94998ec18
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-01-01 12:07:34 -0800

    Fixed controlled to uncontrolled form error

--- src/components/helpers/SemanticField.js
+++ src/components/helpers/SemanticField.js
@@ -4,7 +4,9 @@ import { Field } from "formik";
 // Field wrapper for semantic ui react. Instead of using Field from formik, use this instead.
 // props can be passed through the SemanticField component and will be properly placed in Sem UI component.
 // (Should) work for all types of components
-const SemanticField = ({ component, ...fieldProps }) => (
+
+// Wrapper for anything that's not a field.
+export const SemField = ({ component, ...fieldProps }) => (
   <Field
     {...fieldProps}
     render={({
@@ -32,4 +34,31 @@ const SemanticField = ({ component, ...fieldProps }) => (
   />
 );
 
-export default SemanticField;
+// Wrapper for text fields
+export const SemFieldTxt = ({ component, ...fieldProps }) => (
+  <Field
+    {...fieldProps}
+    render={({
+      field: { value, onBlur, ...field },
+      form: { setFieldValue, setFieldTouched },
+      ...props
+    }) =>
+      React.createElement(component, {
+        ...fieldProps,
+        ...field,
+        ...props,
+        ...(typeof value === 'boolean'
+          ? {
+              checked: value
+            }
+          : {
+              value
+            }),
+        onChange: (e, { value: newValue }) =>
+          setFieldValue(fieldProps.name, newValue),
+        onBlur: (e, blurProps) =>
+          blurProps ? setFieldTouched(fieldProps.name, blurProps.value) : onBlur(e)
+      })
+    }
+  />
+);

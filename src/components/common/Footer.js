130a88be734f560b4e6c2c346cd60f08d36e4afc
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-04-06 16:57:30 -0700

    Minor footer update

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React, { Component } from "react";
 import {
   Container,
   Divider,
@@ -6,64 +6,51 @@ import {
   Header,
   Image,
   List,
-  Segment,
-} from 'semantic-ui-react';
+  Segment
+} from "semantic-ui-react";
+import { withRouter } from "react-router-dom";
 
 class Footer extends Component {
   render() {
     return (
-      <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
-        <Container textAlign='center'>
+      <Segment
+        inverted
+        vertical
+        style={
+          this.props.location.pathname !== "/"
+            ? { margin: "5em 0em 0em", padding: "5em 0em" }
+            : { padding: "5em 0em" }
+        }
+      >
+        <Container textAlign="center">
           <Grid divided inverted stackable>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Group 1' />
+            <Grid.Column width={8}>
+              <Header inverted as="h4" content="Used APIs" />
               <List link inverted>
-                <List.Item as='a'>Link One</List.Item>
-                <List.Item as='a'>Link Two</List.Item>
-                <List.Item as='a'>Link Three</List.Item>
-                <List.Item as='a'>Link Four</List.Item>
+                <List.Item as="a">Google Maps</List.Item>
+                <List.Item as="a">MapQuest</List.Item>
+                <List.Item as="a">Wit.ai</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Group 2' />
-              <List link inverted>
-                <List.Item as='a'>Link One</List.Item>
-                <List.Item as='a'>Link Two</List.Item>
-                <List.Item as='a'>Link Three</List.Item>
-                <List.Item as='a'>Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Group 3' />
-              <List link inverted>
-                <List.Item as='a'>Link One</List.Item>
-                <List.Item as='a'>Link Two</List.Item>
-                <List.Item as='a'>Link Three</List.Item>
-                <List.Item as='a'>Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header inverted as='h4' content='Footer Header' />
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
+
+            <Grid.Column width={8}>
+              <Header inverted as="h4" content="About" />
+              <p>Site created by pixeltopic.</p>
             </Grid.Column>
           </Grid>
 
           <Divider inverted section />
-          <Image centered size='mini' src='https://avatars1.githubusercontent.com/u/32804347?s=400&v=4' />
-          <List horizontal inverted divided link size='small'>
-            <List.Item as='a' href='https://github.com/pixeltopic'>
-              Site Map
+          <Image
+            centered
+            size="mini"
+            src="https://avatars1.githubusercontent.com/u/32804347?s=400&v=4"
+          />
+          <List horizontal inverted divided link size="small">
+            <List.Item as="a" href="https://github.com/pixeltopic">
+              Github
             </List.Item>
-            <List.Item as='a' href='https://github.com/pixeltopic'>
-              Contact Us
-            </List.Item>
-            <List.Item as='a' href='https://github.com/pixeltopic'>
-              Terms and Conditions
-            </List.Item>
-            <List.Item as='a' href='https://github.com/pixeltopic'>
-              Privacy Policy
+            <List.Item as="a" href="https://github.com/pixeltopic">
+              Contact
             </List.Item>
           </List>
         </Container>
@@ -72,4 +59,4 @@ class Footer extends Component {
   }
 }
 
-export default Footer;
+export default withRouter(Footer);

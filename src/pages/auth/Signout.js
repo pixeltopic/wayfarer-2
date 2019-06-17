b70f5cfcbfedea9a30926300324434f69f6f42d7
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-06-17 15:08:50 -0700

    Signout page update

--- src/pages/auth/Signout.js
+++ src/pages/auth/Signout.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Container, Segment, Transition, Header, Icon } from "semantic-ui-react";
+
 import { signout } from "../../actions";
 
 class Signout extends Component {
+
+  _isMounted = false;
+
+  state = { visible: false };
+
   componentDidMount() {
+    this._isMounted = true;
+    
     this.props.signout();
+    setTimeout(() =>  this._isMounted && this.setState({ visible: true }), 500);
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   render() {
-    return <div>You've been logged out.</div>;
+    return (
+      <Container>
+        <Segment padded="very" style={{ minHeight: "80vh" }}>
+          <Transition visible={this.state.visible} animation="fade down" duration={1000}>
+            <Header icon textAlign="center" as="h2" style={{ padding: "20vh" }}>
+              <Icon name="power off" color="red" size="large" circular/>
+              You've been signed out.
+            </Header>
+          </Transition>
+        </Segment>
+      </Container>
+    );
   }
 }
 

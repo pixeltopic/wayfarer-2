364ba5a70a96924d52be2c7c213b662bb6f06ff6
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-06-24 20:36:30 -0700

    Simplify requireAuth HOC as token refreshing will be exclusively handled by the interceptor. Slight refactor to interceptor handler and removed leftover async keyword.

--- src/api/interceptorHandlers.js
+++ src/api/interceptorHandlers.js
@@ -3,21 +3,21 @@ import { updateToken } from "../actions";
 import { AUTH_REFRESHING_TOKEN, AUTH_REFRESHING_CALL } from "../actions/types";
 import server from "./server";
 
+/**
+ * @param {object} config - object possibly containing the `handlerEnabled` key. If false, will not be handled by the interceptor. 
+ */
 const isHandlerEnabled = (config = {}) => {
   return config.hasOwnProperty("handlerEnabled") && !config.handlerEnabled
     ? false
     : true;
 };
 
-export const successHandler = response => {
-  console.log("Response success!");
-  return response;
-};
-
-
-
-const refreshToken = () =>  {
-  // attempts to refresh a token with current user's token and updates global state accordingly.
+/**
+ * Using the current token in the redux store, attempt to refresh token.
+ * Ensures that only one call to api/refreshtoken can be made at once by storing the current promise within the store.
+ * @returns {Promise} refreshingCall - a promise (that may or may not be resolved) to api/refreshtoken. At most one unique api/refreshtoken promise can exist at once.
+ */
+const handlerRefreshToken = () =>  {
   try {
 
     const config = { 
@@ -42,25 +42,29 @@ const refreshToken = () =>  {
     
     return refreshingCall;
 
-  } catch(e) {
-    console.log(e);
+  } catch(error) {
+    console.log(error);
+    return Promise.reject(error);
   }
 }
 
+export const successHandler = response => response;
 
-export const errorHandler = async error => {
-  console.log("error handler");
+export const errorHandler = error => {
 
   const statusCode = error.response ? error.response.status : null;
   const originalRequest = error.config;
 
   if (!isHandlerEnabled(originalRequest)) {
-    console.log("Handler disabled for this response");
+    console.log("Handler disabled for this response.");
     return Promise.reject(error);
   }
 
+  // handlerRefreshToken will always return at most one unique promise to api/refreshtoken. 
+  // Any failed calls block until it resolves, then accesses then .then chain with the new token.
   if (statusCode === 401) {
-    return refreshToken().then(_ => {
+    console.log("Refreshing token in interceptor.");
+    return handlerRefreshToken().then(_ => {
       const refreshedToken = store.getState().auth.authenticated;
       originalRequest.headers["authorization"] = refreshedToken;
       originalRequest.baseURL = undefined;
--- src/components/hocs/requireAuth.js
+++ src/components/hocs/requireAuth.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import history from "../../history";
-import { verifyToken, verifyInactiveToken,routes } from "../../utils";
+// import { verifyToken, verifyInactiveToken,routes } from "../../utils";
 import { refreshToken } from "../../actions";
 
 // HOC to prevent access of component unless authenticated
@@ -17,20 +17,23 @@ export default ChildComponent => {
       this.shouldNavigateAway();
     }
     async shouldNavigateAway() {
-      if (!this.props.auth) { // not logged in at all
-        console.log("requireAuth: Not logged in, redirecting.");
-        history.push(routes.HOME);
-      } else {
-        const prevToken = localStorage.getItem("token");
-        if (!verifyToken(prevToken)) { // token expired
-          if (!verifyInactiveToken(prevToken)) { // check if token can be renewed
-            console.log("requireAuth: Token cannot be refreshed, logging out.");
-            history.push(routes.SIGNOUT);
-            return;
-          }
-          await this.props.refreshToken(); 
-        }
-      }  
+      if (!this.props.auth) {
+        history.push("/");
+      }
+      // if (!this.props.auth) { // not logged in at all
+      //   console.log("requireAuth: Not logged in, redirecting.");
+      //   history.push(routes.HOME);
+      // } else {
+      //   const prevToken = localStorage.getItem("token");
+      //   if (!verifyToken(prevToken)) { // token expired
+      //     if (!verifyInactiveToken(prevToken)) { // check if token can be renewed
+      //       console.log("requireAuth: Token cannot be refreshed, logging out.");
+      //       history.push(routes.SIGNOUT);
+      //       return;
+      //     }
+      //     await this.props.refreshToken(); 
+      //   }
+      // }  
     }
     render() {
       return <ChildComponent {...this.props} />;
